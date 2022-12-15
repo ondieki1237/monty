000260ff13fd03e7892429197476d41ef9019036
ondieki1237 <makoriseth1237@gmail.com>
2022-12-15 00:59:36 -0800

    monty files

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 
 # MONTY
 
-![A test image](/CFYYWy6UEAE9Ow-.png)
-
 ## To study
 * How do I use extern to share variables between source files in C?
 * Stacks and Queues in C
 * Stack operations
 * Queue operations
+
+## concepts covered
+* LIFO and FIFO
+* stack, and when to use it
+* a queue, and when to use it
+* common implementations of stacks and queues
+* the most common use cases of stacks and queues
+* proper way to use global variables
+* working with git submodules
+### Environment
+**ubuntu 20.04LTS**
 
 ### @sethbellarin
---
+++ monty.h
@@ -0,0 +1,154 @@
+#ifndef _MONTY_H_
+#define _MONTY_H_
+
+/* ----- c std lib headers ----- */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <string.h>
+#include <ctype.h>
+
+/* ----- macros ----- */
+
+#define TRUE (1 == 1)
+#define FALSE (!TRUE)
+#define BUFSIZE 1024
+#define LIFO 1
+#define FIFO 0
+
+/* ----- ERRORS ----- */
+
+#define ERROR_MALLOC 0
+#define ERROR_USAGE_FILE 1
+#define ERROR_OPEN_FILE 2
+#define ERROR_UNKNOWN 3
+#define ERROR_PUSH 4
+#define ERROR_PINT 5
+#define ERROR_POP 6
+#define ERROR_SWAP 7
+#define ERROR_ADD 8
+#define ERROR_SUB 9
+#define ERROR_DIV 10
+#define ERROR_DIV_ZERO 11
+#define ERROR_MUL 12
+#define ERROR_MOD 13
+#define ERROR_STACK_EMPTY 14
+#define ERROR_PCHAR_RANGE 15
+
+/* ----- Structs ----- */
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Bootcamp project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Bootcamp project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct inventory_s - a struct pointing to all other structs for this project
+ * @filename: the filename from argv[1]
+ * @stack: pointer to stack
+ * @line: input line received from getline
+ * @input: lines of the files parsed into separate elements
+ * @linenum: the linenumber
+ * @file: the input file
+ * @order: LIFO or FIFO, stack or queue
+ */
+typedef struct inventory_s
+{
+	char *filename;
+	stack_t *stack;
+	char *line;
+	char **input;
+	unsigned int linenum;
+	FILE *file;
+	int order;
+} inventory_t;
+
+/* ----- glbal ----- */
+
+extern inventory_t *inventory;
+inventory_t *inventory;
+
+/* ------Built Inventory -------*/
+
+int build_inventory(void);
+
+/* ----- Add to Stack ----- */
+
+void add_node_lifo(stack_t **stack, stack_t *new_node);
+void add_node_fifo(stack_t **stack, stack_t *new_node);
+void _push(stack_t **stack, unsigned int line_number);
+
+/* ----- Stack ----- */
+
+void _pall(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+
+/* ----- Stack 2 ----- */
+
+void _add(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+void _sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+
+/* ---- Stack 3 ----- */
+
+void _mod(stack_t **stack, unsigned int line_number);
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
+void _rotl(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number);
+
+/* ---- Stack 4 ---- */
+
+void _stack(stack_t **stack, unsigned int line_number);
+void _queue(stack_t **stack, unsigned int line_number);
+
+/* --------- Integer ----------*/
+
+int are_digits(char *num);
+
+/* ----- Execute ------ */
+
+void (*match_opcode(void))(stack_t **stack, unsigned int line_number);
+int parse_line(char *line);
+
+/* ----- Finish ----- */
+
+void free_all(void);
+void free_stack(void);
+void handle_errors(int e);
+
+#endif
